be7419fb13145d4a1819219f6a1473d6224717af
MrTESSARD <dimitri@tessard.pro>
2023-03-02 18:50:08 +0100

    27. mongoose.connect(), mongoose.model()

--- index.js
+++ index.js
@@ -1,18 +1,42 @@
 const PORT = 4000
 const express = require('express')
 const cors = require('cors')
-const app = express()
+const router = express()
 const mongoDBClient = require('./mongoClient')
 
-app.use(cors())
+//API Rest
+const Product = require('./models/product')
+router.get('/products', async (req, res) => {
+  const product = await Product.find({})
+  try {
+    res.send(product)
+    
+  } catch (error) {
+    res.status(500).send(err)
+    
+  }
+})
+router.get('/products/:category', async (req, res) => {
+  const category = req.params.category
+  const product = await Product.find({category})
+  try {
+    res.send(product)
+    
+  } catch (error) {
+    res.status(500).send(err)
+    
+  }
+})
 
-app.get('/', (req, res) => {
+router.use(cors())
+
+router.get('/', (req, res) => {
   res.send('Hello Express! 🎉 ')
 })
 
 
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on PORT ${PORT} `)
+router.listen(PORT, () => {
+  console.log(`Example router listening on PORT ${PORT} `)
   mongoDBClient.initialize()
 })
